6c8044524bcf923970376d248c3c5c46c51b778c
Hergidiangue <135318707+dianguehergi@users.noreply.github.com>
2025-12-24 10:25:52 +0100

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
-import os
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "API vitre-detector OK"
+    return jsonify({
+        "status": "ok",
+        "message": "Vitre detector API is running"
+    })
+
+@app.route("/health")
+def health():
+    return jsonify({"health": "ok"})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8000)
